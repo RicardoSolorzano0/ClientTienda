e0a8fc7e603626885c3c607ea683500e3bfca51e
RicardoSolorzano0 <ricardosolorzano97@gmail.com>
2022-02-01 14:52:37 -0600

    Menu terminado

--- src/components/Admin/MenuTop/MenuTop.js
+++ src/components/Admin/MenuTop/MenuTop.js
@@ -2,13 +2,15 @@ import React from "react";
 import Logo from "../../../assets/img/png/logo.png";
 
 //importacion de elementos de antd
-import { MenuOutlined, PoweroffOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+import { MenuOutlined, PoweroffOutlined, ShoppingCartOutlined, CloseOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 
 //importando los estilos que le daremos al proyecto
 import "./MenuTop.scss";
 
-export default function MenuTop() {
+export default function MenuTop(props) {
+    const { menuCollapsed, setMenuCollapsed } = props;
+
     return (
         <div className="menu-top">
             <div className="menu-top__left">
@@ -17,9 +19,10 @@ export default function MenuTop() {
                     src={Logo}
                     alt="Logo de la pagina"
                 />
-                <Button type="Link" onClick={() => console.log('me hgas')}><MenuOutlined /></Button>
+                <Button type="Link" onClick={() => setMenuCollapsed(!menuCollapsed)}>{menuCollapsed ? <MenuOutlined /> : <CloseOutlined />}</Button>
             </div>
             <div className="menu-top__right">
+
                 <Button type="Link">
                     <PoweroffOutlined />
                 </Button>
@@ -29,6 +32,5 @@ export default function MenuTop() {
 
             </div>
         </div>
-
     );
 }
--- src/layouts/LayoutSales.js
+++ src/layouts/LayoutSales.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "antd";
 
 //importacion para poder enrutar
@@ -9,28 +9,28 @@ import "./LayoutSales.scss";
 
 //importacion de otros componentes
 import MenuTop from "../components/Admin/MenuTop";
+import MenuSider from "../components/Admin/MenuSider";
 
 export default function LayoutSales(props) {
     const { routes } = props;
+    const [menuCollapsed, setMenuCollapsed] = useState(true);
     const { Header, Content, Footer } = Layout;
 
     return (
         <Layout>
-            {/*to do: Menu Sider     revisar si la clase contenedor la necesitaremos*/}
-            <div className="contenedor">
-                <Layout className="layout-sales">
-                    <Header className="layout-sales__header">
-                        <MenuTop />
-                    </Header>
-                    <Content className="layout-sales__content">
-                        <LoadRouters routes={routes} />
-                    </Content>
-                    <Footer className="layout-sales__footer">
-                        © 2022 Ricardo Alberto Solórzano Romero
-                    </Footer>
-                </Layout>
-            </div>
-        </Layout>
+            <MenuSider menuCollapsed={menuCollapsed} />
+            <Layout className="layout-sales" >
+                <Header className="layout-sales__header">
+                    <MenuTop menuCollapsed={menuCollapsed} setMenuCollapsed={setMenuCollapsed} />
+                </Header>
+                <Content className="layout-sales__content" style={{ marginLeft: menuCollapsed ? "0" : "200px" }}>
+                    <LoadRouters routes={routes} />
+                </Content>
+                <Footer className="layout-sales__footer" style={{ marginLeft: menuCollapsed ? "0" : "200px" }}>
+                    © 2022 Ricardo Alberto Solórzano Romero
+                </Footer>
+            </Layout>
+        </Layout >
     );
 }
 
